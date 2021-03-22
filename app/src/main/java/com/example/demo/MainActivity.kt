b747f1d00129c0f356c993f1d2ecf73062af12b1
haidm <haidm@newwave.vn>
2021-03-22 10:38:00 +0700

    added

---
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    // first commit form kudo junior
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
